839cd0b157c2864458ffb11755f0a7e413f47a03
Mateusz0W <153832537+Mateusz0W@users.noreply.github.com>
2024-02-11 15:06:49 +0100

    board

--- include/Board_representation.h
+++ include/Board_representation.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include"SFML/Graphics.hpp"
+#include<string>
+#include<array>
 
 enum Piece{
     Empty,
@@ -16,13 +18,14 @@ enum Colors{
     White=1,
 };
 
-
 class ChessBoard
 {
     private:
         int Board[8][8]{
             {Black*Rook,Black*Knight,Black*Bishop,Black*Queen,Black*King,Black*Bishop,Black*Knight,Black*Rook},
             {Black*Pawn,Black*Pawn,Black*Pawn,Black*Pawn,Black*Pawn,Black*Pawn,Black*Pawn,Black*Pawn,},
+            {Empty,Empty,Empty,Empty,Empty,Empty,Empty,Empty,},
+            {Empty,Empty,Empty,Empty,Empty,Empty,Empty,Empty,},
             {Empty,Empty,Empty,Empty,Empty,Empty,Empty,Empty,},
             {Empty,Empty,Empty,Empty,Empty,Empty,Empty,Empty,},
             {White*Pawn,White*Pawn,White*Pawn,White*Pawn,White*Pawn,White*Pawn,White*Pawn,White*Pawn,},
@@ -32,8 +35,10 @@ class ChessBoard
         bool en_passant_white[8];
         bool castle_black[2];
         bool castle_white[2];
+        std::array<std::string,6> Pieces={"pawn","knight","bishop","rook","queen","king"};
     public:
         void drawChessBoard(sf::RenderWindow& window);
+        void displayPiece(sf::RenderWindow& window,std::string Name,float x, float y);
 };
 
 
--- src/Board_representation.cpp
+++ src/Board_representation.cpp
@@ -1,16 +1,49 @@
 #include "Board_representation.h"
 #include <iostream>
 
+using namespace std;
+
 void ChessBoard::drawChessBoard(sf::RenderWindow& window){
+     //drawing chessboard
      sf::Texture texture;
      if (!texture.loadFromFile("../assets/Chessboard.png"))
      {
-          std::cerr << "Failed to load image." << std::endl;
+          std::cerr << "Failed to load image BOARD." << std::endl;
           exit(EXIT_FAILURE);
      }        
      sf::Sprite sprite;
      sprite.scale(sf::Vector2f(3.f, 3.f));
      sprite.setTexture(texture);
      window.draw(sprite);
-
+     //drawing pieces
+     string NameOfPiece;
+     float x=0.f,y=0.f;
+     for(int i=0;i<8;i++){
+          for(int j=0;j<8;j++){
+               if(Board[i][j]!=0){
+                    if(Board[i][j]>0) //if piece is White
+                         NameOfPiece="W"+Pieces[Board[i][j]-1]+".png"; 
+                    else
+                         NameOfPiece="B"+Pieces[-Board[i][j]-1]+".png"; 
+                    displayPiece(window,NameOfPiece,x,y);
+               }  
+               x+=150.f;
+          } 
+          y+=150.f;
+          x = 0.f;
+     }
+}
+void ChessBoard::displayPiece(sf::RenderWindow& window,string Name,float x, float y){
+     string path="../assets/"+Name;
+     sf::Texture texture;
+     if (!texture.loadFromFile(path))
+     {
+          std::cerr << "Failed to load image PIECE." << std::endl;
+          exit(EXIT_FAILURE);
+     }        
+     sf::Sprite sprite;
+     sprite.scale(sf::Vector2f(0.5f, 0.5f));
+     sprite.setPosition(sf::Vector2f(x,y));//+150.f
+     sprite.setTexture(texture);
+     window.draw(sprite);
 }
